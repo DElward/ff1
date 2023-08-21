d914a0f9f49e9e47320c66888236fedd87d601fb
Dave Elward <dave@taurus.com>
2023-08-21 11:59:32 -0700

    Initial Commit

---
+++ ff1/ff1App.swift
@@ -0,0 +1,17 @@
+//
+//  ff1App.swift
+//  ff1
+//
+//  Created by Dave Elward on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ff1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
